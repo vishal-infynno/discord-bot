9c331b1547f34d1c6849b38f59ce761d08102ebf
Vishal Nirmal <vishal.infynno@gmail.com>
2023-03-20 18:40:28 +0530

    added log statements

--- index.js
+++ index.js
@@ -23,6 +23,8 @@ client.on("messageCreate", async (message) => {
     return;
   }
 
+  console.log(`Received message from ${message.author.tag}`);
+
   const isAuthenticated = !!message.member.roles.cache.find(
     (role) => role.name === "homo-sapiens"
   );
@@ -33,6 +35,7 @@ client.on("messageCreate", async (message) => {
       !!message.mentions.users.find((user) => user.username === "chat-GPT");
 
     if (forGpt) {
+      console.log(`Accessing chatGPT`);
       const response = await axios.post(
         "https://api.openai.com/v1/chat/completions",
         {
@@ -52,6 +55,7 @@ client.on("messageCreate", async (message) => {
           },
         }
       );
+      console.log(`Received response`);
       await message.reply(response.data.choices[0]?.message?.content);
     } else {
       await message.reply(
